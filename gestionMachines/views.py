c1d629285354e1e8d5fbe9c7f124d8a7ca710379
gabouch <gabriel.deramecourt@gmail.com>
2019-04-06 00:05:46 +0200

    Pages index, contact et about avec grand header OK

--- gestionMachines/views.py
+++ gestionMachines/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     context = {}
-    # return HttpResponse("Bonsoir")
     return render(request, 'gestionMachines/index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'gestionMachines/about.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'gestionMachines/contact.html', context)
