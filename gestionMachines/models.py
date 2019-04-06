61422ae6d52195c850c9d13465557841f08c5366
gabouch <gabriel.deramecourt@gmail.com>
2019-04-06 16:54:06 +0200

    Gestion front du questionnaire de création d'user

--- gestionMachines/models.py
+++ gestionMachines/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class MODSOUser(User):
+class MODSOUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nomParrain = models.CharField(max_length=200)
     adresse = models.CharField(max_length=200)
     tel = models.CharField(max_length=200)
--- gestionMachines/views.py
+++ gestionMachines/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .forms import CreateMODSOUserForm
-
 # Page accueil
 def index(request):
     context = {}
@@ -25,8 +23,31 @@ def signin(request):
 
 # Connexion
 def signup(request):
-    form = CreateMODSOUserForm()
-    context = {'form' : form}
+    context = {}
+    if request.method == 'POST':
+        prenom = request.POST.get('prenom')
+        nom = request.POST.get('nom')
+        email = request.POST.get('email')
+        parrain = request.POST.get('parrain')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        check_terms = request.POST.get('check_terms')
+
+        errors = []
+        if not email:
+            errors.append("L'email est obligatoire")
+        if not parrain:
+            errors.append("Le nom du parrain est obligatoire")
+        if not password or password_confirm:
+            errors.append("Le mot de passe est obligatoire")
+        if not password is password_confirm:
+            errors.append("Le mot de passe n'est pas cogérent")
+        
+        success = True
+        message = f"{prenom}, {nom}, {email}, {parrain}, {password}, {password_confirm}, {check_terms}"
+        context = {
+            'errors' : errors
+        }
     return render(request, 'gestionMachines/signup.html', context)
 
 # Post enregistrement utilisateur
@@ -42,10 +63,10 @@ def signupResult(request):
         
         success = True
         message = f"{prenom}, {nom}, {email}, {parrain}, {password}, {password_confirm}, {check_terms}"
-
-    else:
-        success = False
-        message = "Une erreur serveur s'est produite. Pardon pour le dérangement."
+        context = {
+            'success' : success,
+            'message' : message
+        }
     context = {
         'success' : success,
         'message' : message
