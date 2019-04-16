7e2c722432ca0bc946fe37252b4c3376b2cb581e
gabouch <gabriel.deramecourt@gmail.com>
2019-04-16 17:56:34 +0200

    Modif config apps

--- modso2/settings.py
+++ modso2/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gestionMachines.apps.GestionmachinesConfig',
+    'gestionMachines',
 ]
 
 MIDDLEWARE = [
