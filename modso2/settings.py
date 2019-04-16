a8a07d7c6b1014699df3b09b16ed9357bdd47ce6
gabouch <gabriel.deramecourt@gmail.com>
2019-04-16 19:51:29 +0200

    test gestionMachines off

--- modso2/settings.py
+++ modso2/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'ey#qq5$24u$s)kr2elavt$%-1kt*p^^#k(#zh
 # Application definition
 
 INSTALLED_APPS = [
-    'gestionMachines',
+    # 'gestionMachines',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
