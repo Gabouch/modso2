1f4627201dfb910ec512b6864eaff16c05686c6a
gabouch <gabriel.deramecourt@gmail.com>
2019-04-17 14:49:02 +0200

    Modif static

--- modso2/settings.py
+++ modso2/settings.py
@@ -127,14 +127,13 @@ MESSAGE_TAGS = {
 
 django_heroku.settings(locals())
 
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'modso2/media')
 MEDIA_URL = '/media/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
